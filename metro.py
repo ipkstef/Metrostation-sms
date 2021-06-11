9fe749426c5a7845ed9eef6b7711915332603fbe
Stefanos Amanuel <stefano.amanuel@me.com>
2021-06-11 03:15:23 -0400

    I think i got it working for morning

--- metro.py
+++ metro.py
@@ -2,9 +2,13 @@ import schedule
 import time
 import requests 
 import json
+import smtplib, ssl
+from twilio.rest import Client 
 
-API_KEY = '1eef91543ea14395b36b1494aa65dce6'
-REQUEST_URL = 'https://api.wmata.com/StationPrediction.svc/json/GetPrediction/all'
+
+
+
+
 
 CRYSTAL_CITY_CODE = 'C09'
 FRANCONIA_CODE = 'J03'
@@ -13,38 +17,93 @@ def connection_object(stationcode,origin):
 	url = "https://api.wmata.com/StationPrediction.svc/json/GetPrediction/{}".format(stationcode)
 
 	headers = {
-	    "api_key": "1eef91543ea14395b36b1494aa65dce6"    
+	    "api_key": "5773be3b163342cbb78169ba7a43235e"    
 	}
 
-	response = requests.get(url, headers=headers) 
-	hello = response.json()
+
+	try:
+		r = requests.get(url, headers=headers) 
+		r.raise_for_status()
+		hello = r.json()
+		# connection_object.hello2 = r.content 
+	except requests.exceptions.HTTPError as err:
+		raise SystemExit(err)
 
 
-	for station in hello["Trains"]:
-		print("These trains are on the way to "+ origin +"! \n" "Destination Name: " +station["DestinationName"], "\n" "Arrival Min: "+station["Min"], "\n" "Line Color: " + station["Line"], "\n" "--------")
-
-
-
-
-
-
-
-
-
-
-
-
-
+	try:
+		for station in hello["Trains"]:
+			connection_object.train_list = str("These trains are on the way to "+ origin +"! \n" "Destination Name: " +station["DestinationName"], "\n" "Arrival Min: "+station["Min"], "\n" "Line Color: " + station["Line"], "\n" "--------")
+			return connection_object.train_list			
+	except requests.exceptions.InvalidURL as err2:
+		raise SystemExit(err2)
 
 
 
 
-# schedule.every(5).seconds.do(work)
-schedule.every(4).seconds.do(connection_object, stationcode=FRANCONIA_CODE, origin='Work')
+
+
+def send_text():
+ 
+	account_sid = 'AC108acf465f89df469fd0de6d7d669e64' 
+	auth_token = '4a80f0b19c25c72e4c24af37a53d28d0' 
+	client = Client(account_sid, auth_token) 
+
+	message = client.messages.create(  
+	                              messaging_service_sid='MGb7941e6389915d027f73898201fadb62', 
+	                              body= '{}'.format(connection_object(FRANCONIA_CODE,"Work")),      
+	                              to='+17038324707' 
+	                          ) 
+	 
+	print(message.sid)
+
+
+
+
+
+
+
+
+
+# # send text via email
+
+# # Establish a secure session with gmail's outgoing SMTP server using your gmail account
+# server = smtplib.SMTP( "smtp.gmail.com", 587 )
+
+# server.starttls()
+
+# server.login( '@gmail', 'password' )
+
+# body = 'Im alive!'
+# message = ("From: %s\n" % 'stefano.amanuel@gmail.com' + "To: %s\r\n" % '7038324707@tmomail.net' + "Subject: %s\r\n" % 'Metro Arrival Times' + "\r\n" + body)
+
+# # Send text message through SMS gateway of destination number
+# server.sendmail( 'FROM', '@tmomail.net', message )
+
+
+
+
+
+
+
+
+
+for i in ["08:00", "08:15", "08:30", "08:45", "09:00"]:
+    schedule.every().monday.at(i).do(send_text)
+    schedule.every().tuesday.at(i).do(send_text)
+    schedule.every().wednesday.at(i).do(send_text)
+    schedule.every().thursday.at(i).do(send_text)
+    schedule.every().friday.at(i).do(send_text)
+
+
+
+
+
+
+
 
 
 
 
 while True:
 	schedule.run_pending()
-	time.sleep(1)
+	time.sleep(15)
